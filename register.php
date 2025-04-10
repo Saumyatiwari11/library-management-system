eed411b38ba40a02580d9823cc84f12126988bbc
Saumyatiwari11 <138548286+Saumyatiwari11@users.noreply.github.com>
2025-04-10 09:17:51 +0530

    Create register.php

---
+++ register.php
@@ -0,0 +1,38 @@
+<?php
+include 'db_connect.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $query = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+    if (mysqli_query($conn, $query)) {
+        header("Location: index.php");
+        exit();
+    } else {
+        $error = "Username already taken!";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Register</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Register</h1>
+        <?php if (isset($error)) echo "<p class='error'>$error</p>"; ?>
+        <form method="POST">
+            <label>Username:</label>
+            <input type="text" name="username" required><br>
+            <label>Password:</label>
+            <input type="password" name="password" required><br>
+            <button type="submit">Register</button>
+        </form>
+        <p>Already have an account? <a href="index.php">Login here</a></p>
+    </div>
+</body>
+</html>
